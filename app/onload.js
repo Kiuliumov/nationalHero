ab9a652fedeffc8200fa923a9dac19b04e1b2b04
Kiuliumov <79997432+Kiuliumov@users.noreply.github.com>
2024-11-13 07:18:30 +0200

    app

--- app/onload.js
+++ app/onload.js
@@ -3,15 +3,23 @@ import getData  from "./getdata.js"
 
 async function giveID(){
     const id = getIdFromUrl()
-    if (id > 8){
+    if (id = 'all'){
         const p = document.getElementsByTagName('p')[0]
-        p.innerHTML = 'Id not found.'
-        return;
-
+        const data = await getData(id);
+        p.innerHTML = JSON.stringify(data);
+        
+    } else {
+        if (id >= 8){
+            const p = document.getElementsByTagName('p')[0]
+            p.innerHTML = 'Id not found.'
+            return;
+    
+        }
+    
+        const p = document.getElementsByTagName('p')[0]
+        const data = await getData(id);
+        p.innerHTML = JSON.stringify(data);
     }
-    const p = document.getElementsByTagName('p')[0]
-    const data = await getData(id);
-   p.innerHTML = JSON.stringify(data);
     
 }
 
