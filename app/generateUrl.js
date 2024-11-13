45d05b4551e9a7513946c8fa8784e4adb6beacee
Kiuliumov <79997432+Kiuliumov@users.noreply.github.com>
2024-11-13 07:07:06 +0200

    national

---
+++ app/generateUrl.js
@@ -0,0 +1,9 @@
+function generateUrl(id){
+    const url = new URL('http://127.0.0.1:5500/template.html');
+    url.searchParams.append('id', id)
+    return url.toString();
+}
+
+module.exports = generateUrl;
+
+console.log(generateUrl(1));
