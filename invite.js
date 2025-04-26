eafbd9bdd24ff61a8d21c00bd8ff0b3ff537524e
Kiuliumov <79997432+Kiuliumov@users.noreply.github.com>
2025-04-26 11:00:27 +0300

    Invite

--- invite.js
+++ invite.js
@@ -15,4 +15,20 @@ function changeButtonColor() {
     colorCycle = !colorCycle; 
 }
 
+function buttonOnClick() {
+    const img = document.querySelector('img');
+    
+    img.src = 'https://i.ibb.co/xKWxPmGt/Copy-of.png';
+    confetti({
+        particleCount: 100,
+        spread: 70,
+        origin: { y: 0.6 }
+    });
+}
+
+buttons.forEach(button => {
+    button.addEventListener('click', buttonOnClick);
+});
+
+
 setInterval(changeButtonColor, 3000);
